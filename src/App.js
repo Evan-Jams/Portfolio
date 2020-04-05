421973ee6d96a0c6b8bc11fe994478076072f492
Evan James <thevan777@gmail.com>
2020-04-05 17:57:39 -0400

    styled a little more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Nav from './components/Nav.js'
 import Home from './components/Home.js'
 import About from './components/About.js'
@@ -17,12 +17,13 @@ class App extends Component {
             <Nav />
           </div>
           <div className="header">
-            <div id="header-img">
-              
+            <div id="header-img"></div>
+            <div id="personal-photo"></div>
+            <div className="right-header">
+              <h1>Evan James</h1>
+              <p>Software Engineer | Golf Professional</p>
+              <button href=".main-container"><Link to="/about">About Me</Link></button>
             </div>
-            <div id="personal-photo">
-            </div>
-            <h1>Evan James</h1>
           </div>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
