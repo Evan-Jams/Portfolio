26047f635454d59cf460ccae70006ea139c750cd
Evan James <thevan777@gmail.com>
2020-04-06 14:11:10 -0400

    added some info on the home page and centered the header image

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,10 @@ class Home extends Component {
     return(
       <>
         <div className="main-container">
-          <h2>Home Page</h2>
+          <h1>Welcome To My Website</h1>
+          <div className="content">
+            <p>My name is Evan James. I am a full-stack web developer, and a former Golf Professional. Still new to the coding world, I bring a fresh mind and a hunger to improve everyday. General Assembly's Software Engineering Bootcamp and their outstanding team of instructors have opened up a brand new world of opportunity through Web Development. I have a passion for creating the best user experience possible, while keeping things simple and clean. Always on the search for new web technologies and features.</p>
+          </div>
         </div>
       </>
     )
