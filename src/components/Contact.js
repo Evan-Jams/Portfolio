5e058a2e4647a539b81a7ef30ffb161c094ac588
Evan James <thevan777@gmail.com>
2020-04-14 22:03:38 -0400

    mobile formatted

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,8 +8,9 @@ class Contact extends Component {
         <div className="main-container">
           <h1>Check Out My Resume</h1>
           <div className="resume">
-
-            <img src="/resume.png" alt="screenshot of resume" />
+            <a href="https://docs.google.com/document/d/1CZtq71wlyIJdHy3PzRXjPtZwWUMjNiRcA-YWvx0Odaw/edit">
+              <img src="/resume.png" alt="screenshot of resume" />
+            </a>
           </div>
           <h2>Contact Me</h2>
           <Form />
